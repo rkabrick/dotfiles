f0563c8bb13619bf6559b5554e60e35fed119d87
Ryan Kabrick <rkabrick@udel.edu>
2023-12-24 14:37:39 -0500

    Adding ctrl-a & ctrl-e bindings to zsh-vim-mode because old habits die hard

--- zsh/.config/zsh/keymaps.zsh
+++ zsh/.config/zsh/keymaps.zsh
@@ -1,1 +1,4 @@
 bindkey -s '^F' 'tmux-sessionizer\n'
+# Allow zsh-vim-mode to still use ctrl-a / ctrl-e to navigate to the beginning-of-line and end-of-line, respectively
+bindkey -M viins '^A' beginning-of-line
+bindkey -M viins '^E' end-of-line
