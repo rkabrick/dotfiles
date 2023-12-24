7aed19ef4fbe08677c15a71f6dcafbd1a09b1e54
Ryan Kabrick <rkabrick@udel.edu>
2023-12-24 14:37:22 -0500

    Making fzf look better & modifying RISCV env vars

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,9 +4,11 @@ export PATH=/opt/homebrew/bin:$PATH
 export PATH=/opt/homebrew/sbin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.local/bin/scripts:$PATH
+export PATH=/usr/local/bin:$PATH
 export FZF_DEFAULT_OPTS="
-  --color=bg:#1a1b26,fg:#c0caf5,hl:#7aa2f7
-  --color=bg+:#414868,fg+:#c0caf5,hl+:#bb9af7
+  --color=bg:#26283F,fg:#c0caf5,hl:#7aa2f7
+
+  --color=bg+:#353A59,fg+:#c0caf5,hl+:#bb9af7
   --color=info:#7dcfff,prompt:#7dcfff,pointer:#ff007c
   --color=marker:#9ece6a,spinner:#7dcfff,header:#444b6a
 "
@@ -23,6 +25,6 @@ export TASKDATA=~/.config/task
 
 # RISC-V
 export RISCV="$HOME/.local/opt/riscv"
-export RVCC="$RISCV/bin/riscv64-unknown-elf-gcc"
-export RVCXX="$RISCV/bin/riscv64-unknown-elf-g++"
+export RVCC="$RISCV/bin/riscv64-unknown-elf-gcc-12.2.0"
+export RVCXX="$RISCV/bin/riscv64-unknown-elf-g++-12.2.0"
 
